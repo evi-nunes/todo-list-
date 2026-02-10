cf8384e15deddb99ac5f3f257ff947057fa9603c
Evila <66744375+evi-nunes@users.noreply.github.com>
2026-02-10 15:58:26 -0300

    Create list item on button click
    Add functionality to create a new list item on button click.

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const botao = document.getElementById("adicionar");
 const lista = document.getElementById("lista");
 botao.addEventListener("click", function () {
 const texto = input.value;
+const item = document.createElement("li");
 
 });
