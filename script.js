c9770ad6ff22320c76436fe115a4fd139e0b5a26
Evila <66744375+evi-nunes@users.noreply.github.com>
2026-02-10 15:55:12 -0300

    Add input value retrieval on button click
    Add event listener for button click to retrieve input value.

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const input = document.getElementById("tarefa");
 const botao = document.getElementById("adicionar");
 const lista = document.getElementById("lista");
 botao.addEventListener("click", function () {
+const texto = input.value;
 
 });
